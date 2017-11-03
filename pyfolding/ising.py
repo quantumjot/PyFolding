e121c0d74d0371789c4be96a7aa28d4f22d14af4
quantumjot <code@arlowe.co.uk>
2017-11-03 17:53:09 +0000

    Moved Ising plotting back to Ising module

--- pyfolding/ising.py
+++ pyfolding/ising.py
@@ -33,9 +33,9 @@ import matplotlib.pyplot as plt
 from scipy.optimize import differential_evolution, minimize, leastsq, curve_fit
 
 # import PyFolding specific libraries
+import core
 import constants
 import models
-import core
 from plotting import *
 
 __author__ = "Alan R. Lowe"
@@ -808,6 +808,257 @@ def fit_heteropolymer(equilibrium_curves=[], topologies=[], popsize=10, tol=1e-8
 
 	return results
 
+"""
+ISING PLOTTING FUNCTIONS
+"""
+
+def plot_Ising(fit_func):
+	"""
+	Function to plot fitted Ising model data.
+
+	"""
+
+	cmap = ['ro', 'mo', 'go', 'co', 'bo', 'ko', 'rv', 'mv', 'gv', 'cv', 'bv',
+			'kv', 'rs', 'ms', 'gs', 'cs', 'bs', 'ks', 'r.', 'm.', 'g.', 'c.',
+			'b.', 'k.']
+
+	# plot the fits
+	plt.figure(figsize=(14,8))
+	ax1 = plt.subplot2grid((2,2), (0,0), rowspan=2)
+
+	for protein in fit_func.proteins:
+		idx = fit_func.proteins.index(protein)
+		xv = protein['curve'].x
+		ax1.plot(xv, protein['curve'].y, cmap[idx % len(cmap)], ms=4)
+
+	#plt.legend( [p['curve'].ID for p in fit_func.proteins], loc='upper left')
+
+	for protein in fit_func.proteins:
+		idx = fit_func.proteins.index(protein)
+		xv = np.linspace(0., np.max(protein['curve'].x), 100)
+		ax1.plot(xv, protein['partition'].theta(xv), cmap[idx % len(cmap)][0]+'-',
+			lw=2, label=protein['curve'].ID)
+
+	ax1.set_xlabel(fit_func.proteins[0]['curve'].denaturant_label)
+	ax1.set_ylabel('Fraction unfolded')
+
+
+	# now plot the first derivative
+	pk_max = []
+	ax2 = plt.subplot2grid((2,2), (0,1), rowspan=1)
+	for protein in fit_func.proteins:
+		idx = fit_func.proteins.index(protein)
+		xv = np.linspace(0.,10.,1000)
+		first_deriv = np.gradient(protein['partition'].theta(xv))
+		pk_max.append( (protein['n'], xv[np.argmax(np.abs(first_deriv))]) )
+		ax2.plot(xv, np.abs(first_deriv), cmap[idx % len(cmap)][0]+'-', lw=2,
+			label=protein['curve'].ID)
+
+	ax2.set_xlabel(fit_func.proteins[0]['curve'].denaturant_label)
+	ax2.set_ylabel('First derivative of fit function')
+
+
+	ax3 = plt.subplot2grid((2,2), (1,1), rowspan=1)
+	h,x = plot_folded(fit_func.proteins[-1]['partition'])
+	dn = iter(['{0:s}_{1:d}'.format(d.name,i) for i,d in enumerate(fit_func.proteins[-1]['partition'].topology)])
+	for i in xrange(h.shape[1]):
+		plt.plot(x, h[:,i], cmap[i % len(cmap)]+'-', lw=2, markersize=4,
+			label=dn.next())
+	ax3.set_xlabel(fit_func.proteins[0]['curve'].denaturant_label)
+	ax3.set_ylabel('Fraction unfolded (subpopulation)')
+
+	# do some formatting with the smaller plots to fit legends beside them
+	# http://stackoverflow.com/questions/4700614/how-to-put-the-legend-out-of-the-plot
+	box2 = ax2.get_position()
+	ax2.set_position([box2.x0, box2.y0, box2.width*0.8, box2.height])
+	box3 = ax3.get_position()
+	ax3.set_position([box3.x0, box3.y0, box3.width*0.8, box3.height])
+
+	# Put a legend below current axis
+	ax2.legend(loc='center left', bbox_to_anchor=(1., 0.5))
+	ax3.legend(loc='center left', bbox_to_anchor=(1., 0.5))
+
+	plt.show()
+
+
+
+
+def plot_folded(partition):
+	h = np.zeros((100,partition.n))
+	x = np.linspace(0.,10.,100)
+	for i in xrange(partition.n):
+		p = partition.subpopulation(x,i)
+		h[:,i] = p
+	return h,x
+
+
+def collapse_topology(topology):
+	"""
+	Collapse a topology into a compact representation.
+	"""
+
+	# make a copy so that we don't do this in place
+	topology_full = list(topology)
+	compact = []
+	counter = 0
+
+	domain = topology_full.pop(0)
+
+	while topology_full:
+		next_domain = topology_full.pop(0)
+		if next_domain is domain:
+			counter += 1
+		else:
+			compact.append((domain, counter+1))
+			counter = 0
+			domain = next_domain
+
+	# clean up
+	compact.append((domain, counter+1))
+
+	return compact
+
+
+
+
+def brace(x,y, rev=1, length=0.95):
+	xb = [x+(rev*.4), x+(rev*.5), x+(rev*.5), x+(rev*.4)]
+	yb = [y+length/2, y+length/2, y-length/2, y-length/2]
+	return xb,yb
+
+
+
+
+def plot_domains(topologies, labels=None, collapse=False, **kwargs):
+	"""
+	Function to generate a pretty plot of the domain architecture of
+	the various protein topologies presented.
+
+
+	Args:
+		topologies: the protein topologies
+		labels: protein names
+		fold: a boolean that collapses sequences of similar domains for smaller
+			representation.
+
+	"""
+
+
+	if not isinstance(topologies, list):
+		raise TypeError("Topologies must be a list of topologies.")
+
+	# first we need to check whether the topologies are instantiated classes or
+	# not. If they are already instantiated, then we need to recover the
+	# underlying classes...
+	if isinstance(topologies[0][0], IsingDomain):
+		tmp_topologies = []
+		for t in topologies:
+			new_topology = [d.__class__ for d in t]
+			tmp_topologies.append(new_topology)
+	else:
+		tmp_topologies = topologies
+
+	from matplotlib.patches import Patch
+
+	if not labels:
+		labels = ['Protein {0:d}'.format(i) for i in xrange(len(tmp_topologies))]
+
+	if 'fold' in kwargs:
+		raise DeprecationWarning('Fold keyword is being replaced with collapse.')
+		collapse = kwargs['fold']
+
+
+	# collapse the topologies
+	compact = []
+	domain_types = set()
+
+	for l,t in zip(labels, tmp_topologies):
+
+		domain_types.update([d().name for d in t])
+
+		c = collapse_topology(t)
+		if not collapse:
+			tc = []
+			for d,n in c: tc += [(d,1)]*n
+			c = tc
+
+		compact.append((l, c))
+
+
+	# set up the plotting
+	domain_types = list(domain_types)
+
+	d_color = lambda name: cmap[ domain_types.index(name) ]
+	cmap = ['r', 'k', 'g', 'b', 'c', 'm', 'y']
+
+	# now we want to plot these
+	plt.figure(figsize=(14, len(tmp_topologies)*1.5))
+	ax = plt.subplot(111)
+
+	for y, (protein, topology) in enumerate(compact):
+		for x, (domain, d_cnt) in enumerate(topology):
+
+			name = domain().name
+			c = plt.Circle((x*1.5, y), 0.45, edgecolor=d_color(name),
+						facecolor='w', label=name)
+	 		ax.add_artist(c)
+
+
+	 		# if we're folding, then plot braces to show that
+	 		if d_cnt > 1:
+	 			lb_x, lb_y = brace(x*1.5,y, rev=-1)
+	 			rb_x, rb_y = brace(x*1.5,y, rev=1)
+	 			b = plt.Line2D(lb_x, lb_y, color='k')
+	 			ax.add_artist(b)
+	 			b = plt.Line2D(rb_x, rb_y, color='k')
+	 			ax.add_artist(b)
+	 			ax.text(x*1.5+.55, y-.4, str(d_cnt), color='k')
+
+
+	 	 	i_str = str(x)
+			ij_str = str(x)
+
+			# add on any labels:
+			if hasattr(domain(), 'm_i'):
+				ax.text(x*1.5,y+.1,'$m^{'+i_str+'}_{i}$',
+					horizontalalignment='center', fontsize=18, color=d_color(name))
+			if hasattr(domain(), 'DG_i'):
+				ax.text(x*1.5,y-.3,'$\Delta G^{'+i_str+'}_{i}$',
+					horizontalalignment='center', fontsize=18, color=d_color(name))
+
+			# draw arrows to represent interactions
+			if hasattr(domain(), 'DG_ij') and x!=len(topology)-1:
+				ax.text(x*1.5+.9,y,'$\Delta G^{'+ij_str+'}_{ij}$',
+					horizontalalignment='center', fontsize=12, rotation=90,
+					color=d_color(name))
+
+
+
+	ax.set_yticks(np.arange(len(tmp_topologies)), minor=False)
+	ax.set_xticklabels([], minor=False, rotation='vertical')
+	ax.set_yticklabels(labels, minor=False)
+
+	# make the legend entries
+	l = [ Patch(edgecolor=d_color(d), facecolor='w', label=d) for d in domain_types ]
+
+
+	ax.set_xlim([-1.,11.])
+	ax.set_ylim([-1., len(tmp_topologies)])
+	ax.set_aspect('equal', adjustable='box')
+	plt.legend(handles=l, loc='lower right')
+	plt.title('Ising model domain topologies')
+
+	if 'save' in kwargs:
+		save_filename = kwargs['save']
+		if not isinstance(save_filename, basestring):
+			raise TypeError("Save path must be a string")
+		if not save_filename.endswith(('.pdf', '.PDF')):
+			save_filename = save_filename+".pdf"
+
+		plt.savefig(save_filename, dpi=144., pad_inches=0)
+
+	plt.show()
+	return
 
 
 if __name__ == "__main__":
